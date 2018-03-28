d95081d84c990ffaad0cc56a05c4f0b9beeac47c
KEVIN NEIL BAUTISTA <bautistakevinneil@gmail.com>
2018-03-28 21:09:46 +0800

    I am only beginner.

---
+++ aboutme.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="mainstyle.css">
+	<link rel="stylesheet" type="text/css" href="aboutme.css">
+    <title>Online Resume</title>
+  </head>
+  <body>
+
+		<nav class="navbar  navbar-expand-lg ">
+
+
+		<div class="container-fluid">
+		<ul class="navbar-nav mx-auto">
+			<li class="nav-item">
+			<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="aboutme.php">About Me</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="education.php">Educational Background</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="workexperience.php">Work Experience</a>
+			</li >
+			<li class="nav-item">
+			<a class="nav-link" href="skills.php">Skills</a>
+			</li>
+
+
+		</ul>
+		</div>
+		</nav>
+
+		<div class="background">
+		<div class="personal-information">
+
+		<h1>About Me</h1>
+
+		<table class="table table-stripped">
+			<tr>
+
+			<th colspan="3">Personal Information</th>
+			</tr>
+			<tr>
+			<td>NAME</td>
+			<td>KEVIN NEIL P. BAUTISTA</td>
+			</tr>
+			<tr>
+			<td>ADDRESS</td>
+			<td>LOT H PHASE 1 BLK. 35 WELFAREVILLE COMPOUND ADDITION HILLS MANDALUYONG CITY METRO MANILA PHILLIPINES</td>
+			</tr>
+			<tr>
+			<td>AGE</td>
+			<td>26 Y/O</td>
+			</tr>
+			<tr>
+			<td>BIRTHDAY</td>
+			<td>AUGUST 29, 1991</td>
+			</tr>
+			<tr>
+			<td>BIRTHPLACE</td>
+			<td>DORONGAN ST. DEL CARMEN SAGÑAY CAMARINES, SUR PHILLIPINES</td>
+			</tr>
+			<tr>
+			<td>CIVIL STATUS</td>
+			<td>SINGLE</td>
+			</tr>
+			<tr>
+			<td>CONTACT NUMBER</td>
+			<td>(02)880-2399</td>
+			</tr>
+			<tr>
+			<td>PERSON TO CONTACT IN CASE OF EMERGENCY</td>
+			<td>JESSE C. PEQUEÑA</td>
+			</tr>
+			<tr>
+			<td>CONTACT NUMBER</td>
+			<td>09213114858</td>
+			</tr>
+			<tr>
+			<td>RELATIONSHIP</td>
+			<td>UNCLE</td>
+			</tr>
+
+		</table>
+
+
+
+		</div>
+		</div>
+
+
+  </body>
+</html>
---
+++ workexperience.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	
+	 <!-- Optional JavaScript -->
+    
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="mainstyle.css">
+	<link rel="stylesheet" type="text/css" href="aboutme.css">
+	<link rel="stylesheet" type="text/css" href="workexperience.css">
+	
+    <title>Online Resume</title>
+  </head>
+  <body>
+  
+
+	
+	<nav class="navbar  navbar-expand-lg ">
+		
+		
+		<div class="container-fluid">
+		<ul class="navbar-nav mx-auto">
+			<li class="nav-item">
+			<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="aboutme.php">About Me</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="education.php">Educational Background</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="workexperience.php">Work Experience</a>
+			</li >
+			<li class="nav-item">
+			<a class="nav-link" href="skills.php">Skills</a>
+			</li>
+			
+		
+		</ul>
+		</div>
+		</nav>
+		
+		
+		
+		<div class="background">
+		<div class="personal-information">
+		
+		<h1>Work Experience</h1>
+		<table class="table table-bordered">
+		<th>Company</th>
+		<th>Position</th>
+		<th>Start Date</th>
+		<th>End Date</th>
+		<tr>
+		<td>KFC CORPORATION</td>
+		<td>SERVICE CREW DINING</td>
+		<td>NOVEMBER 2009</td>
+		<td>OCTOBER 2010</td>
+		</tr>
+		<tr>
+		<td>SPI CRM</td>
+		<td>CSR / TSR</td>
+		<td>DECEMBER 2016</td>
+		<td>MAY 2017</td>
+		</tr>
+		<tr>
+		<td>TECHCELLAR BUSSINESS SOLUTIONS</td>
+		<td>WORDPRESS OPERATOR</td>
+		<td>FEBRUARY 2018</td>
+		<td></td>
+		</tr>
+		
+		</table>
+		
+		
+		
+		
+		
+		
+		</div>
+		</div>
+	
+ 
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+  </body>
+</html>
